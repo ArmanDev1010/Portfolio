1cf00e0d09124c921df8708b4a5621a78bc9e9c1
ArmanDev1010 <arman.manukyan24.y@gmail.com>
2024-11-06 18:17:34 +0400

    changed footer mistake

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
             <a href="#hero">
               <img src={logo} alt="" className="w-[400px]" />
             </a>
-            <div className="flex items-center gap-[1rem] justify-end">
+            <div className="flex items-center gap-[1rem] justify-end max-small:flex-col">
               <a
                 href="tel:+37498627272"
                 className="gap-[0.75rem] text-white tracking-[0.02rem] whitespace-nowrap flex-nowrap px-[2rem] text-[1rem] font-[700] no-underline flex capitalize border-[1.5px] border-[#1a1a1e] rounded-[50vw] justify-center items-center p-[1rem] transition-border duration-[0.2s] hover:border-white"
@@ -23,20 +23,22 @@ const Footer = () => {
                 <FiPhone className="w-[24px] h-[24px]" />
                 <div className="">+374 98 62 72 72</div>
               </a>
-              <a
-                href="https://www.facebook.com/profile.php?id=100092271615808"
-                target="_blank"
-                className="text-white border-[1.5px] border-[#1a1a1e] rounded-[50vw] p-[1rem] pl-[1.1rem] transition-border duration-[0.2s] hover:border-white"
-              >
-                <BsFacebook className="w-[24px] h-[24px]" />
-              </a>
-              <a
-                href="mailto:arman.manukyan4.y@gmail.com"
-                target="_blank"
-                className="text-white border-[1.5px] border-[#1a1a1e] rounded-[50vw] p-[1rem] pl-[1.1rem] transition-border duration-[0.2s] hover:border-white"
-              >
-                <TbMail className="w-[26px] h-[26px]" />
-              </a>
+              <div className="flex gap-[1rem]">
+                <a
+                  href="https://www.facebook.com/profile.php?id=100092271615808"
+                  target="_blank"
+                  className="text-white border-[1.5px] border-[#1a1a1e] rounded-[50vw] p-[1rem] pl-[1.1rem] transition-border duration-[0.2s] hover:border-white"
+                >
+                  <BsFacebook className="w-[24px] h-[24px]" />
+                </a>
+                <a
+                  href="mailto:arman.manukyan4.y@gmail.com"
+                  target="_blank"
+                  className="text-white border-[1.5px] border-[#1a1a1e] rounded-[50vw] p-[1rem] pl-[1.1rem] transition-border duration-[0.2s] hover:border-white"
+                >
+                  <TbMail className="w-[26px] h-[26px]" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
